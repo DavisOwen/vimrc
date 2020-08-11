42c862c70597c7b56bb3a69512f3423dc388cad6
Davis Owen <sdowen12@gmail.com>
2020-08-10 23:44:15 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/dense-analysis/ale.git
+git clone https://github.com/tpope/vim-commentary.git
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+git clone https://github.com/junegunn/fzf.vim.git
+git clone https://github.com/morhetz/gruvbox.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/tpope/vim-fugitive.git
+git clone https://github.com/sheerun/vim-polyglot.git
+~/.fzf/install
